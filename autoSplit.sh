65954f6c3647c0c0e5734a9e249b52d10c677ca4
kwy <kwyoung04@naver.com>
2022-08-18 15:21:20 +0900

    check count

--- autoSplit.sh
+++ autoSplit.sh
@@ -2,22 +2,24 @@
 echo "Automatic annotation data split"
 
 
-for file in `find . -name '*.zip'`;
-do
-  data=`find -wholename ${file}*`;
-  echo "${data}";
+#for file in `find . -name '*.zip'`;
+#do
+#  data=`find -wholename ${file}*`;
+#  echo "${data}";
 
-  if [ $data -z ];then
-    continue
-  fi
+#  if [ $data -z ];then
+#    continue
+#  fi
 
   #echo " unzip "${data}" -d "${data:0:-4}" ";
-  unzip "${data}" -d "${data:0:-4}";
-done
+#  unzip "${data}" -d "${data:0:-4}";
+#done
 
 find . -name 'instances_default.json' -exec python3 coco_class_add.py {} \;
 find . -name '0st.json' -exec python3 coco_split.py {} \;
 
 
+find -wholename '*/split/*.json' | wc -l
+find -name '*jpg' | wc -l
 
 echo "Operation complete"

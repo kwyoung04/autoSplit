a2cde5f14a61ccda45670841f46344237c547105
kwyoung04 <59862512+kwyoung04@users.noreply.github.com>
2022-09-28 17:04:34 +0900

    Update autoSplit.sh

--- autoSplit.sh
+++ autoSplit.sh
@@ -2,18 +2,18 @@
 echo "Automatic annotation data split"
 
 
-for file in `find . -name '*.zip'`;
-do
-  data=`find -wholename ${file}*`;
-  echo "data: ${data}";
+#for file in `find . -name '*.zip'`;
+#do
+#  data=`find -wholename ${file}*`;
+#  echo "data: ${data}";
 
-  if [ $data -z ];then
-    continue
-  fi
+#  if [ $data -z ];then
+#    continue
+#  fi
 
-  #echo " unzip "${data}" -d "${data:0:-4}" ";
-  unzip "${data}" -d "${data:0:-4}";
-done
+#  #echo " unzip "${data}" -d "${data:0:-4}" ";
+#  unzip "${data}" -d "${data:0:-4}";
+#done
 
 find . -name 'instances_default.json' -exec python3 coco_class_add.py {} \;
 find . -name '0st.json' -exec python3 coco_split.py {} \;

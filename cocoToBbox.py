89097e8b9d4ee591c9e08ac6b900ba62ff8ec764
kwy <kwyoung04@naver.com>
2022-09-28 17:03:24 +0900

    Add to bbox

---
+++ cocoToBbox.py
@@ -0,0 +1,55 @@
+from email.errors import StartBoundaryNotFoundDefect
+import json
+import argparse
+import os 
+
+import sys
+
+STUFF_LIST = {151, 152, 153, 154, 156, 157, 158, 159, 160, 155, 161}
+
+
+def run(jsonPath, splitSavePath):
+    with open(jsonPath, 'rt', encoding='UTF-8-sig') as data:
+        coco = json.load(data)
+        annotations = coco['annotations']
+        lenAn = len(annotations)
+        i = 0
+        
+        while (i < lenAn):
+            try:
+                del(annotations[i]['segmentation'])
+            except KeyError:
+                pass
+
+            for stuff in STUFF_LIST:
+                if annotations[i]['category_id'] == stuff:
+                    del(annotations[i])
+                    lenAn = lenAn -1
+                    i = i-1
+
+            i = i+1     
+
+
+        coco['annotations'] = annotations
+
+    save_coco(splitSavePath, coco)
+
+def save_coco(file, data):
+    with open(file, 'wt', encoding='UTF-8') as coco:
+        json.dump(data, coco, indent=2, sort_keys=False, ensure_ascii = False)
+
+
+if __name__ == "__main__":
+    argument = sys.argv[1]
+    
+    basename = os.path.basename(argument)
+    abspath = os.path.abspath(argument)
+    print(argument)
+
+    if basename != "instances_default.json":
+        exit()
+    
+    splitSavePath = argument[:-5] + "_bbox.json"
+
+    run(argument,splitSavePath)
+

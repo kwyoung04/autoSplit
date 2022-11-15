e2425b95009a9f7875ddbfaba052cda8f12356b9
kwy <kwyoung04@naver.com>
2022-11-15 18:10:21 +0900

    deleting stuff class

--- cocoToBbox.py
+++ cocoToBbox.py
@@ -5,7 +5,8 @@ import os
 
 import sys
 
-STUFF_LIST = {151, 152, 153, 154, 156, 157, 158, 159, 160, 155, 161}
+#STUFF_LIST = {151, 152, 153, 154, 156, 157, 158, 159, 160, 155, 161}
+STUFF_LIST = {161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151}
 
 
 def run(jsonPath, splitSavePath):
@@ -17,21 +18,24 @@ def run(jsonPath, splitSavePath):
         
         while (i < lenAn):
             try:
-                #del(annotations[i]['segmentation'])
+            #del(annotations[i]['sZegmentation'])
                 annotations[i]['segmentation'] = []
             except KeyError:
                 pass
 
-            for stuff in STUFF_LIST:
-                if annotations[i]['category_id'] == stuff:
-                    del(annotations[i])
-                    lenAn = lenAn -1
-                    i = i-1
+            if annotations[i]['category_id'] in STUFF_LIST:
+                del(annotations[i])
+                lenAn = lenAn -1
+                i = i-1
 
             i = i+1     
 
 
         coco['annotations'] = annotations
+        
+        for stuff in STUFF_LIST:
+            del(coco['categories'][150])
+
 
     save_coco(splitSavePath, coco)
 

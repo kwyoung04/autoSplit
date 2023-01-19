4e514071a090052c2cfb24a1b6b254591a308b38
kwy <kwyoung04@naver.com>
2023-01-20 00:00:47 +0900

    empty_id_only_toBbox

--- cocoToBbox.py
+++ cocoToBbox.py
@@ -6,12 +6,38 @@ import os
 import sys
 
 #STUFF_LIST = {151, 152, 153, 154, 156, 157, 158, 159, 160, 155, 161}
-STUFF_LIST = {161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151}
-
+#STUFF_LIST = {161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151}
+STUFF_LIST = [152, 153, 154, 155, 156, 157, 145, 158, 98, 99, 100]
 
 def run(jsonPath, splitSavePath):
     with open(jsonPath, 'rt', encoding='UTF-8-sig') as data:
         coco = json.load(data)
+        STUFF_LIST.sort(reverse=True)
+                
+        #duffi = dict.fromkeys(STUFF_LIST)
+        #print(duffi)
+        duffi = []
+        
+        
+        cate = coco['categories']
+        lenCat = len(cate)
+        i = 0
+        cnt = 0
+        while (i < lenCat):
+            if coco['categories'][i]['id'] in STUFF_LIST:
+                #print("del stuff:", coco['categories'][i])
+                del(coco['categories'][i])
+                cnt += 1
+                lenCat -=1
+                continue
+            
+            #print(coco['categories'][i]['id'], coco['categories'][i]['id']-cnt)
+            duffi.append(coco['categories'][i]['id'])
+            coco['categories'][i]['id'] = coco['categories'][i]['id']-cnt
+            i+=1
+        coco['categories'] = cate
+        #print(duffi)
+                
         annotations = coco['annotations']
         lenAn = len(annotations)
         i = 0
@@ -27,16 +53,14 @@ def run(jsonPath, splitSavePath):
                 del(annotations[i])
                 lenAn = lenAn -1
                 i = i-1
-
+            else:    
+                #print(annotations[i]['category_id'], duffi.index(annotations[i]['category_id'])+1)
+                annotations[i]['category_id'] = duffi.index(annotations[i]['category_id'])+1
             i = i+1     
 
 
         coco['annotations'] = annotations
         
-        for stuff in STUFF_LIST:
-            del(coco['categories'][150])
-
-
     save_coco(splitSavePath, coco)
 
 def save_coco(file, data):
